77a74bc3bafd66a3cca96ec765d12a3bc99238c7
Atif <iamatif96@gmail.com>
2024-07-02 07:21:28 +0800

    changes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,8 +1,9 @@
 import Post from './Post';
+import classes from './PostList.module.css'
 
 function PostList(){
   return(
-    <ul>
+    <ul className={classes.posts}>
       <Post author='Atif' body='Full Stack developer'/>
       <Post author='Resume' body='The details about the Candidate'/>
     </ul>
