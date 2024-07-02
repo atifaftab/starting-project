f8ba4e99185dd4ee2c3b4ab230c0a1dd9a9b793e
Atif <iamatif96@gmail.com>
2024-07-02 22:36:58 +0800

    newPost component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostList.module.css'
 
 function PostList(){
   return(
+    <>
+    <NewPost/>
     <ul className={classes.posts}>
       <Post author='Atif' body='Full Stack developer'/>
       <Post author='Resume' body='The details about the Candidate'/>
     </ul>
+    </>
   );
 }
 export default PostList
